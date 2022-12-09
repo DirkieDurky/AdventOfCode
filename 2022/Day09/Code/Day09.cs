34e31ca50872959fd78cf9c4ff71f941ef3da56b
Dirk Freijters <dirk@freijters.nl>
2022-12-09 10:21:19 +0100

    Start for day 9

---
+++ 2022/Day09/Code/Day09.cs
@@ -0,0 +1,28 @@
+using System.Drawing;
+namespace Year2022
+{
+    public class Day09 : IDay
+    {
+        public Object Sol1(String input)
+        {
+            String[] lines = input.Split('\n');
+
+            Point headPoint = new Point(0, 0);
+            Point tailPoint = new Point(0, 0);
+
+            foreach (String line in lines)
+            {
+
+            }
+
+            return "";
+        }
+
+        public Object Sol2(String input)
+        {
+
+
+            return "";
+        }
+    }
+}
