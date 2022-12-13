e885c3fe959566132c3026a2b65223e0cc8757cf
DirkieDurky <dirk@freijters.nl>
2022-12-13 02:43:16 +0100

    Day 12 (Still not done)

--- 2022/Day12/Code/Day12.cs
+++ 2022/Day12/Code/Day12.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Drawing;
 
 namespace Year2022
@@ -11,20 +12,37 @@ namespace Year2022
         {
             List<String> lines = input.Split('\n').ToList();
             Map = lines;
-            const String heightLookup = "abcdefghijklmnopqrstuvwxyz";
 
             Int32 startY = Map.FindIndex(x => x.Contains("S"));
             Int32 startX = Map[startY].IndexOf("S", StringComparison.Ordinal);
             Node start = new(startX, startY);
 
+            StringBuilder sb = new(Map[startY]);
+            sb[startX] = 'a';
+            Map[startY] = sb.ToString();
+
             Int32 endY = Map.FindIndex(x => x.Contains("E"));
             Int32 endX = Map[startY].IndexOf("E", StringComparison.Ordinal);
             Node end = new(endX, endY);
 
+            sb = new(Map[endY]);
+            sb[endX] = 'z';
+            Map[endY] = sb.ToString();
+
             List<Node> activeNodes = new();
             List<Node> visitedNodes = new();
 
+            activeNodes.Add(start);
+            activeNodes.Add(end);
 
+            while (activeNodes.Any()){
+                Node checkNode = activeNodes.MaxBy(x=>x.CostDistance)!;
+
+                if (checkNode.X == endX && checkNode.Y == endY){
+                    
+                }
+            }
+            
             // Console.WriteLine("Shortest route:");
             // for (Int32 i = 0; i < shortestRoute.Count; i++)
             // {
@@ -46,8 +64,20 @@ namespace Year2022
                 Point adjacentPoint = new(currentNode.X + direction.DeltaX, currentNode.Y + direction.DeltaY);
                 if (adjacentPoint.X >= Map[0].Length || adjacentPoint.X < 0 ||
                     adjacentPoint.Y >= Map.Count || adjacentPoint.Y < 0) continue;
-                Int32 height;
+
+                const String heightLookup = "abcdefghijklmnopqrstuvwxyz";
+                Char c = Map[currentNode.Y][currentNode.X];
+                Int32 currentNodeHeight = heightLookup.IndexOf(c);
+
+                c = Map[adjacentPoint.Y][adjacentPoint.X];
+                Int32 adjacentNodeHeight = heightLookup.IndexOf(c);
+
+                if (adjacentNodeHeight > currentNodeHeight + 1) continue;
+
+                walkableNodes.Add(new Node(adjacentPoint.X + direction.DeltaX,adjacentPoint.Y + direction.DeltaY,currentNode.Cost + 1,currentNode));
             }
+
+
         }
 
         public Object Sol2(String input)
