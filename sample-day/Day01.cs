b11dfbc047614727c04a97d9152105f7ab3af8eb
DirkieDurky <dirk@freijters.nl>
2023-12-01 14:25:17 +0100

    Added sample-day for copying every day

---
+++ sample-day/Day01.cs
@@ -0,0 +1,19 @@
+namespace Year2023
+{
+    public class Day01 : IDay
+    {
+        public Object Sol1(String input)
+        {
+
+
+            return "result";
+        }
+
+        public Object Sol2(String input)
+        {
+
+
+            return "result";
+        }
+    }
+}
