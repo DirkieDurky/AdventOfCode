6f2211a385d31aa0a57ed53dcc945dec21d355c7
DirkieDurky <dirk@freijters.nl>
2023-12-03 00:05:10 +0100

    Day02

---
+++ 2023/Day02/Code/Day02.cs
@@ -0,0 +1,114 @@
+namespace Year2023
+{
+    public class Day02 : IDay
+    {
+        public class Game
+        {
+            public int ID;
+            public List<Set> sets = new();
+        }
+
+        public class Set
+        {
+            public int redCount = 0;
+            public int greenCount = 0;
+            public int blueCount = 0;
+        }
+
+        public static List<Game> Parse(String input)
+        {
+            String[] lines = input.Split("\n");
+            List<Game> games = new();
+
+            foreach (String game in lines)
+            {
+                if (game == "") continue;
+
+                String[] split = game.Split(": ");
+
+                Game newGame = new()
+                {
+                    ID = int.Parse(split[0].Split(" ")[1])
+                };
+
+                String[] sets = split[1].Split("; ");
+
+                foreach (String set in sets)
+                {
+                    String[] cubeAmounts = set.Split(", ");
+                    Set newSet = new();
+
+                    foreach (String cubeAmount in cubeAmounts)
+                    {
+                        String[] cube = cubeAmount.Split(" ");
+
+                        switch (cube[1])
+                        {
+                            case "red":
+                                newSet.redCount = int.Parse(cube[0]);
+                                break;
+                            case "green":
+                                newSet.greenCount = int.Parse(cube[0]);
+                                break;
+                            case "blue":
+                                newSet.blueCount = int.Parse(cube[0]);
+                                break;
+                        }
+                    }
+
+                    newGame.sets.Add(newSet);
+                }
+
+                games.Add(newGame);
+            }
+
+            return games;
+        }
+
+        public Object Sol1(String input)
+        {
+            int sum = 0;
+            List<Game> games = Parse(input);
+
+            foreach (Game game in games)
+            {
+                // Console.WriteLine(game.ID);
+                // Console.WriteLine("{");
+                // foreach (Set set in game.sets)
+                // {
+                //     Console.WriteLine("    {");
+                //     Console.WriteLine("        Red: " + set.redCount);
+                //     Console.WriteLine("        Green: " + set.greenCount);
+                //     Console.WriteLine("        Blue: " + set.blueCount);
+                //     Console.WriteLine("    }");
+                // }
+
+                if (!game.sets.Any(x => x.redCount > 12 || x.greenCount > 13 || x.blueCount > 14))
+                {
+                    sum += game.ID;
+                    // Console.WriteLine("Possible");
+                }
+                // else
+                // {
+                //     Console.WriteLine("Impossible");
+                // }
+                // Console.WriteLine();
+            }
+
+            return sum;
+        }
+
+        public Object Sol2(String input)
+        {
+            int sum = 0;
+            List<Game> games = Parse(input);
+
+            foreach (Game game in games)
+            {
+                sum += game.sets.MaxBy(x => x.redCount)!.redCount * game.sets.MaxBy(x => x.greenCount)!.greenCount * game.sets.MaxBy(x => x.blueCount)!.blueCount;
+            }
+
+            return sum;
+        }
+    }
+}
