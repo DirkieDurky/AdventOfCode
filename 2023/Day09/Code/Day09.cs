520c98f867c1314d9734b43567de3c3c7ac8e0ac
DirkieDurky <dirk@freijters.nl>
2023-12-09 15:12:29 +0100

    Day09

---
+++ 2023/Day09/Code/Day09.cs
@@ -0,0 +1,79 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Year2023
+{
+    public class Day09 : IDay
+    {
+        public Object Sol1(String input)
+        {
+            int GetPrediction(List<int> inputMap)
+            {
+                List<int> resultMap = new();
+
+                if (inputMap.All(x => x == 0))
+                {
+                    return 0;
+                }
+                else
+                {
+                    for (int i = 0; i < inputMap.Count - 1; i++)
+                    {
+                        resultMap.Add(inputMap[i + 1] - inputMap[i]);
+                    }
+
+                    return inputMap.Last() + GetPrediction(resultMap);
+                }
+            }
+
+            String[] lines = input.Split('\n');
+            List<int[]> histories = lines.Select(x => x.Split(" ").Select(int.Parse).ToArray()).ToList();
+
+            int total = 0;
+
+            foreach (int[] history in histories)
+            {
+                int prediction = GetPrediction(history.ToList());
+                // Console.WriteLine(prediction);
+                total += prediction;
+            }
+
+            return total;
+        }
+
+        public Object Sol2(String input)
+        {
+            int GetPrediction(List<int> inputMap)
+            {
+                List<int> resultMap = new();
+
+                if (inputMap.All(x => x == 0))
+                {
+                    return 0;
+                }
+                else
+                {
+                    for (int i = 0; i < inputMap.Count - 1; i++)
+                    {
+                        resultMap.Add(inputMap[i + 1] - inputMap[i]);
+                    }
+
+                    return inputMap.First() - GetPrediction(resultMap);
+                }
+            }
+
+            String[] lines = input.Split('\n');
+            List<int[]> histories = lines.Select(x => x.Split(" ").Select(int.Parse).ToArray()).ToList();
+
+            int total = 0;
+
+            foreach (int[] history in histories)
+            {
+                int prediction = GetPrediction(history.ToList());
+                // Console.WriteLine(prediction);
+                total += prediction;
+            }
+
+            return total;
+        }
+    }
+}
